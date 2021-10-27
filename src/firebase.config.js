d8902ed805def428b8e1f8193625e28a7f8a838f
aidilrx04 <mrcrayv14@gmail.com>
2021-10-27 11:41:30 +0800

    releases 0.1.2 - Upload firebase config for Heroku deployment

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDaaf8MytbOCQ-yLeVQr5DoUeumKq6E8Hw",
+    authDomain: "scramble-me-40e8f.firebaseapp.com",
+    projectId: "scramble-me-40e8f",
+    storageBucket: "scramble-me-40e8f.appspot.com",
+    messagingSenderId: "474157322031",
+    appId: "1:474157322031:web:f15f13c94fb6ef8fdc1bda"
+};
+
+export default firebaseConfig;
